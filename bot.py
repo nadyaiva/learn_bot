ed2f490afbff12287bd00324f5c2b73d08936bcb
Nadya Ivanova <nadya.ivanova.vla@gmail.com>
2021-02-25 19:46:22 +0100

    my first bot

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
 
 logging.basicConfig(filename='bot.log', level=logging.INFO)
 
@@ -13,7 +14,7 @@ def talk_to_me(update, context):
     update.message.reply_text(text)
 
 def main():
-    mybot = Updater("1586604330:AAHjowPZsc8ZiAIjr-f0ftulQfFGMIac4JY", use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
@@ -23,4 +24,5 @@ def main():
     mybot.start_polling()
     mybot.idle()
 
-main()
+if __name__ == "__main__":
+    main()
