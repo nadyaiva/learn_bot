2cb3645280e02e3d0b9e4ae8a17cf9901164efe5
Nadya Ivanova <nadya.ivanova.vla@gmail.com>
2021-02-28 21:01:58 +0100

    Добавляю модуль Ephem в бота

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import ephem
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import settings
@@ -13,11 +14,22 @@ def talk_to_me(update, context):
     print(text)
     update.message.reply_text(text)
 
+def handle_planet(update, context):
+    text = update.message.text
+    getPlanet = text.split(' ')
+    nameofplanet = getPlanet[1]
+
+    if nameofplanet == 'mars':
+        mars = ephem.Mars('2000/01/01')
+        constellation = ephem.constellation(mars)
+        update.message.reply_text(constellation)
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", handle_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал")
